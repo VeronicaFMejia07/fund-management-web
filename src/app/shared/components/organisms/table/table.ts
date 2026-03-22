1c2cb46d42b7453eafc2cebf875eb6b58c045b54
VeronicaFMejia07 <mveronicamejia842@gmail.com>
2026-03-22 18:52:10 -0500

    Creación de organismo reutilizable Table

---
+++ src/app/shared/components/organisms/table/table.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, ContentChild, Input, TemplateRef } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+
+export interface TableColumn<T> {
+  field: keyof T | string;
+  header: string;
+  pipe?: 'currency' | 'date' | 'number';
+}
+
+@Component({
+  selector: 'app-organism-table',
+  standalone: true,
+  imports: [TableModule, CommonModule],
+  templateUrl: './table.html',
+  styleUrl: './table.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableOrganismComponent<T> {
+  @Input() columns: TableColumn<T>[] = [];
+  @Input() data: T[] = [];
+  @Input() showActions = true;
+  @ContentChild('actions') actionTemplate?: TemplateRef<any>;
+
+  public selectedRow: T | null = null;
+}

7fdf10d3675b4f9b2f9c9e344c28705082648885
VeronicaFMejia07 <mveronicamejia842@gmail.com>
2026-03-22 18:49:22 -0500

    Creación del router y estilos globales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'funds',
+    pathMatch: 'full',
+  },
+  {
+    path: 'funds',
+    loadComponent: () => import('./pages/funds/funds').then(m => m.FundsComponent),
+  },
+  {
+    path: 'subscriptions',
+    loadComponent: () => import('./pages/subscriptions/subscriptions').then(m => m.SubscriptionsComponent),
+  },
+  {
+    path: 'history',
+    loadComponent: () => import('./pages/history/history').then(m => m.HistoryComponent),
+  }
+
+];

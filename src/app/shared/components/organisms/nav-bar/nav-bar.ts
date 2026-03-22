34c2009a091e6d5187a0a37a0030765fa50ec896
VeronicaFMejia07 <mveronicamejia842@gmail.com>
2026-03-22 18:52:27 -0500

    Creación de organismo reutilizable NavBar

---
+++ src/app/shared/components/organisms/nav-bar/nav-bar.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { IconAtomComponent, LabelAtomComponent } from "@shared/atoms";
+import { NavLinkMoleculeComponent } from "../../molecules/nav-link/nav-link";
+
+@Component({
+  selector: 'app-organism-nav-bar',
+  standalone: true,
+  imports: [MenubarModule, IconAtomComponent, LabelAtomComponent, NavLinkMoleculeComponent],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavBarOrganismComponent {
+  @Input() items: MenuItem[] = [
+    {
+      label: 'Fondos',
+      routerLink: '/funds',
+    },
+    {
+      label: 'Mis suscripciones',
+      routerLink: '/subscriptions',
+    },
+    {
+      label: 'Historial',
+      routerLink: '/history',
+    },
+  ];
+}

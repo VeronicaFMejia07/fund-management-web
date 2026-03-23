f123bda4d3aa1654a3fac11fa1b4a4440bd3918a
VeronicaFMejia07 <mveronicamejia842@gmail.com>
2026-03-23 02:21:50 -0500

    Crear archivo de base de datos para Json-Server y configuraciones en el App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarOrganismComponent } from '@shared/organisms';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarOrganismComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('fund-management-web');
+  public menuItems:MenuItem[] = [
+    {
+      label: 'Fondos',
+      routerLink: '/funds',
+    },
+    {
+      label: 'Mis suscripciones',
+      routerLink: '/subscriptions',
+    },
+    {
+      label: 'Historial',
+      routerLink: '/history',
+    },
+  ]
 }

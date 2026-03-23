dd68dd15cc064703260a04ccdd2c55fe228479c5
VeronicaFMejia07 <mveronicamejia842@gmail.com>
2026-03-22 23:23:14 -0500

    Instalación y configuración de SweetAlert

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import localeEsCo from '@angular/common/locales/es-CO';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(localeEsCo); // Registramos el locale para español de Colombia
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'es-CO' }
   ]
 };
